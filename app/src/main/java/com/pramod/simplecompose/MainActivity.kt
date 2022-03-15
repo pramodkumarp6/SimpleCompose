7caa4c1a843043a12e4df4c3c4ceb806b2d9703a
Pramod kumar <pramodkumarp6@gmail.com>
2022-03-15 14:22:20 +0530

    hi

--- app/src/main/java/com/pramod/simplecompose/MainActivity.kt
+++ app/src/main/java/com/pramod/simplecompose/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SimpleComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -31,13 +30,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Pramod $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SimpleComposeTheme {
-        Greeting("Android")
+        Greeting("Kumar")
     }
 }
